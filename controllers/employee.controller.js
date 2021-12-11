132f23259fea359d15e46329d8e4ea8c542a45f0
abdulvahap.gencdal <abdulvahap.gencdal@accenture.com>
2021-12-11 03:33:19 +0200

    remain functions added

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -7,12 +7,20 @@ export const findAllEmployees = (req, res)=>{
             message:
             err.message || "Some error occurred while retrieving employees."
         });
-        else res.send(data);
+        else 
+            res.send(data);
     });
 }
 
 export const findAllPromotedEmployees =(req, res)=>{
-    res.send("users");
+    EmployeeDao.findAllPromoted((err, data) => {
+        if (err)
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while retrieving employees."
+          });
+        else res.send(data);
+      });
 }
 
 // Find a single Employee by Id
@@ -35,17 +43,102 @@ export const findOneEmployee = (req, res)=>{
 }
 
 export const createEmployee =(req, res)=>{
-    res.send("users");
+    // Validate request
+    if (!req.body) {
+        res.status(400).send({
+        message: "Content can not be empty!"
+        });
+    }
+
+    //here you should also check length of string
+    if (!req.body.lastName||
+        !req.body.firstName||
+        !req.body.email||
+        !req.body.salary||
+        !req.body.jobTitle) {    
+            res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    // Create a Employee
+    const employee = new EmployeeDao({
+        lastName : req.body.lastName,
+        firstName : req.body.firstName,
+        email : req.body.email,
+        salary : req.body.salary,
+        jobTitle : req.body.jobTitle,
+        promoted : req.body.promoted?1:0
+    });
+
+  // Save Employee in the database
+    EmployeeDao.create(employee, (err, data) => {
+        if (err)
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while creating the Employee."
+        });
+        else res.send(data);
+    });
 }
 
 export const updateEmployee =(req, res)=>{
-    res.send("users");
+   // Validate request
+   if (!req.body) {
+        res.status(400).send({
+        message: "Content can not be empty!"
+        });
+    }
+
+    //here you should also check length of string
+    if (!req.body.lastName||
+        !req.body.firstName||
+        !req.body.email||
+        !req.body.salary||
+        !req.body.jobTitle) {    
+            res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    EmployeeDao.updateById(req.params.id,new EmployeeDao(req.body),(err, data)=>{
+        if (err) {
+            if (err.kind === "not_found") {
+              res.status(404).send({
+                message: `Not found Employee with id ${req.params.id}.`
+              });
+            } else {
+              res.status(500).send({
+                message: "Error updating Employee with id " + req.params.id
+              });
+            }
+          } else res.send(data);
+    });
 }
 
 export const deleteEmployee =(req, res)=>{
-    res.send("users");
+    EmployeeDao.remove(req.params.id, (err, data) => {
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: `Not found Employee with id ${req.params.id}.`
+            });
+          } else {
+            res.status(500).send({
+              message: "Could not delete Employee with id " + req.params.id
+            });
+          }
+        } else res.send({ message: `Employee was deleted successfully!` });
+      });
 }
 
 export const deleteAllEmployees =(req, res)=>{
-    res.send("users");
+    EmployeeDao.removeAll((err, data) => {
+        if (err)
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while removing all employees."
+          });
+        else res.send({ message: `All employees were deleted successfully!` });
+      });
 }
--- models/employee.model.js
+++ models/employee.model.js
@@ -1,12 +1,11 @@
 import connection from './database.js';
-import mysql from 'mysql';
+import { v4 as uuidv4 } from 'uuid';
 
 // constructor
 const EmployeeDao = function(employee) {
     this.lastName = employee.lastName;
     this.firstName = employee.firstName;
     this.email = employee.email;
-    this.employeeCode = employee.employeeCode;
     this.salary = employee.salary;
     this.jobTitle = employee.jobTitle;
     this.promoted = employee.promoted;
@@ -44,5 +43,87 @@ EmployeeDao.findAll = (result)=>{
         result(null, res);
     });
 };
+
+EmployeeDao.findAllPromoted = (result)=>{
+    //if there is many parameters : var params  = {id: 1, title: 'Hello MySQL'}; --> pass params to query
+    connection.query(`SELECT * FROM employees WHERE promoted=1`, (err, res)=>{
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        
+        console.log("found employees: ", res);
+        result(null, res);
+    });
+};
+
+EmployeeDao.create = (employee, result)=>{
+    const sql = "INSERT INTO employees(`lastName`,`firstName`,`employeeCode`,`email`,`salary`,`jobTitle`,`promoted`) VALUES (?,?,?,?,?,?,?)";
+    const values = [employee.lastName,employee.firstName,'x'+ uuidv4().substring(1,7),employee.email,employee.salary,employee.jobTitle,employee.promoted];
+    connection.query(sql, values, (err, res)=>{
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+          }
+          console.log(res);
+          //console.log("created employee: ", { id: res.insertId, ...employee });
+          result(null, { id: res.insertId, ...employee });
+    });
+}
+
+EmployeeDao.updateById = (id, employee, result) => {
+    const sql =  "UPDATE employees SET lastName = ?, firstName = ?, email = ?, salary = ?, jobTitle = ?, promoted = ? WHERE id = ?";
+    const values = [employee.lastName,employee.firstName,employee.email,employee.salary,employee.jobTitle,employee.promoted,id];
+    connection.query(sql, values, (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(null, err);
+          return;
+        }
+  
+        if (res.affectedRows == 0) {
+          // not found Employee with the id
+          result({ kind: "not_found" }, null);
+          return;
+        }
+  
+        console.log("updated employee: ", { id: id, ...employee });
+        result(null, { id: id, ...employee });
+      });
+  };
+
+  EmployeeDao.remove = (id, result) => {
+    sql.query("DELETE FROM employees WHERE id = ?", id, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      if (res.affectedRows == 0) {
+        // not found EMployee with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+  
+      console.log("deleted employee with id: ", id);
+      result(null, res);
+    });
+  };
+  
+  EmployeeDao.removeAll = result => {
+    sql.query("DELETE FROM employees", (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+  
+      console.log(`deleted ${res.affectedRows} employees`);
+      result(null, res);
+    });
+  };
 
 export default EmployeeDao;
